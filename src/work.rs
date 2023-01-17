bccbd8fef2497ba32aeb2ef0e3e4291cc802bc15
Damon L Montague Jr <damon@montague.im>
2023-01-17 06:18:21 -0500

    added simd to test (nightly is needed)

--- src/work.rs
+++
@@ -1,7 +0,0 @@
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread;
-use std::time::{Duration, Instant};
-use sysinfo::System;
-use crate::reporting::watch_in_background;
-
